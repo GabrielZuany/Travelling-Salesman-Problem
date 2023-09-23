bdb5a0aa02687473f725e77695b7c54699a67658
GabrielZuany <gzuanydev@gmail.com>
2023-09-22 22:58:31 -0300

    Update trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -1,20 +1,22 @@
-# run randon.py
-# run make rand
-# run profiler.py
-
 echo "Running trigger.sh"
 echo "Running random.py"
-python ./RandomInputs/rand.py
+python3 "RandomInputs/rand.py"
 
 echo "Running make rand"
 make read_test
+
+for file in ./Others/exemplos/exemplos/in/*.tsp
+do
+    echo "Running $file"
+    ./trab1 $file
+done
 
 echo "Running main..."
 for file in ./RandomInputs/*.tsp
 do
     echo "Running $file"
-    ./main $file
+    ./trab1 $file
 done
 
 echo "Running profiler.py"
-python ./Profiler/profiler.py
+python3 ./Profiler/profiler.py
